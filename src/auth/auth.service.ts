4527f122addcfc20749d11baa220be603aaab350
João Lucas <sstecnologiainformacao@gmail.com>
2025-02-16 15:57:13 -0300

    more users

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,7 +3,15 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 @Injectable()
 export class AuthService {
   private readonly users = [
-    { username: 'user', password: 'user', role: 'user' },
+    { username: 'user1', password: 'user', role: 'user' },
+    { username: 'user2', password: 'user', role: 'user' },
+    { username: 'user3', password: 'user', role: 'user' },
+    { username: 'user4', password: 'user', role: 'user' },
+    { username: 'user5', password: 'user', role: 'user' },
+    { username: 'user6', password: 'user', role: 'user' },
+    { username: 'user7', password: 'user', role: 'user' },
+    { username: 'user8', password: 'user', role: 'user' },
+    { username: 'user9', password: 'user', role: 'user' },
     { username: 'admin', password: 'admin', role: 'admin' },
   ];
 
