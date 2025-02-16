a9e225904facbc7eaff108ecbbe824ab93e05783
João Lucas <sstecnologiainformacao@gmail.com>
2025-02-16 14:09:33 -0300

    fix CORS problem

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000', 
+      "https://bioverse-frontend.vercel.app",
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
